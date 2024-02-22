b5b9b71617d6403f96cfb21637940c6832b6e710
robbe.vanderkerken <robbe.vanderkerken@student.hogent.be>
2024-02-22 11:46:30 +0100

    match results and standing done

--- transfermarkt.py
+++ transfermarkt.py
@@ -1,3 +1,5 @@
+import re
+
 import csv
 
 import requests
@@ -14,10 +16,11 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 seasons = []
 matches = []
+ranking_data = []
 
 for seasonObj in soup.find_all('div', class_='inline-select'):
     for season in seasonObj.find_all('option'):
-        if int(season['value']) >= 1000:
+        if int(season['value']) >= 2023:
             seasons.append(season['value'] if season else None)
 
 for season in seasons:
@@ -36,6 +39,7 @@ for season in seasons:
         soup = BeautifulSoup(response.text, 'html.parser')
 
         matches_table = soup.find('h1').find_next('table')
+        rankings_table = soup.find('table', class_='items')
 
         last_date = None
 
@@ -56,6 +60,11 @@ for season in seasons:
                 home_team = cols[4].get_text(strip=True)
                 away_team = cols[9].get_text(strip=True)
                 result = cols[6].get_text(strip=True)
+                match_id_tag = soup.find('a', href=re.compile(r'/spielbericht/index/spielbericht/'))
+                if match_id_tag and match_id_tag['href']:
+                    match_id = re.search(r'/spielbericht/index/spielbericht/(\d+)', match_id_tag['href'])
+                    if match_id:
+                        extracted_id = match_id.group(1)
                 # for true last_date cut off first 2 char in CSV
                 if result == time:
                     result = ''
@@ -65,14 +74,54 @@ for season in seasons:
                     matches.append(
                         {'date': last_date[2:], 'time': time, 'home_team': home_team[idx_home + 1:],
                          'result_home_team': result[:-2], 'result_away_team': result[2:],
-                         'away_team': away_team[:idx_away]})
+                         'away_team': away_team[:idx_away],
+                         'season': season,
+                         'day': day,
+                         'match_id': extracted_id})
+
+        for row in rankings_table.find_all('tr')[1:]:
+            cols = row.find_all('td')
+
+            rank = cols[0].get_text(strip=True).split()[0]
+            club_name = cols[2].get_text(strip=True)
+            played = cols[3].get_text(strip=True)
+            wins = cols[4].get_text(strip=True)
+            draws = cols[5].get_text(strip=True)
+            losses = cols[6].get_text(strip=True)
+            goals = cols[7].get_text(strip=True)
+            goal_difference = cols[8].get_text(strip=True)
+            points = cols[9].get_text(strip=True)
+
+            ranking_data.append({
+                'Rank': rank,
+                'Club': club_name,
+                'Played': played,
+                'Wins': wins,
+                'Draws': draws,
+                'Losses': losses,
+                'Goals': goals,
+                'Goal Difference': goal_difference,
+                'Points': points,
+                'Season': season,
+                'Day': day
+            })
 
 csv_file = "csv/match_results.csv"
 with open(csv_file, 'w', newline='', encoding='utf-8') as file:
     writer = csv.DictWriter(file, fieldnames=['date', 'time', 'home_team', 'result_home_team', 'result_away_team',
-                                              'away_team'])
+                                              'away_team', 'season', 'day', 'match_id'])
     writer.writeheader()
     for match in matches:
         writer.writerow(match)
+
+print(f"Data written to {csv_file}")
+
+csv_file = "csv/standings.csv"
+with open(csv_file, 'w', newline='', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fieldnames=['Rank', 'Club', 'Played', 'Wins', 'Draws',
+                                              'Losses', 'Goals', 'Goal Difference', 'Points', 'Season', 'Day'])
+    writer.writeheader()
+    for ranking in ranking_data:
+        writer.writerow(ranking)
 
 print(f"Data written to {csv_file}")

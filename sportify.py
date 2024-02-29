b8261e44991a651cd85e2a2a4ee4b28c2705f4a2
Tom Deganck <tom.deganck@student.hogent.be>
2024-02-29 10:25:51 +0100

    tijd sportify geherformateerd

--- sportify.py
+++ sportify.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import time
 import pandas as pd
 
-fieldnames = ['date','time','Event ID', 'Event Name', 'Starts At', 'Home Team', 'Away Team']
+fieldnames = ['date','time','Event ID', 'Event Name','Play day','Start hour', 'Home Team', 'Away Team']
 
 
 # URL en headers
@@ -39,10 +39,13 @@ def process_data(data):
     for event in events:
         # Get the variables 
         odds = []
-        fieldnames = ['date','time','Event ID', 'Event Name', 'Starts At', 'Home Team', 'Away Team']
+        fieldnames = ['date','time','Event ID', 'Event Name','Play day','Start hour', 'Home Team', 'Away Team']
         event_id = event.get('id')
         event_name = event.get('name')
         starts_at = event.get('starts_at')
+        time_match = datetime.strptime(starts_at,"%Y-%m-%dT%H:%M:%S.%fZ")
+        start_dag = time_match.strftime("%d %b. %Y")
+        start_uur = time_match.strftime("%H:%M")
         home_team = event.get('home_team')
         away_team = event.get('away_team')
         markets = event.get('markets', [])
@@ -55,7 +58,7 @@ def process_data(data):
                 odds.append(outcome.get('display_odds', {}).get('decimal')) # Get the odd in decimal form
         dt = datetime.fromtimestamp(time.time()).strftime("%d %b. %Y")
         ds = datetime.fromtimestamp(time.time()).strftime("%H:%M")
-        match = [dt,ds,event_id,event_name,starts_at,home_team,away_team] + odds # Make all the data per event into 1 list
+        match = [dt,ds,event_id,event_name,start_dag,start_uur,home_team,away_team] + odds # Make all the data per event into 1 list
         matches.append(match)
     return matches,fieldnames
     

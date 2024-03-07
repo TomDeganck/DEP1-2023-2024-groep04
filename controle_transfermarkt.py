64d15138cd12f1c85269796fd8360084de876a49
robbe.vanderkerken <robbe.vanderkerken@student.hogent.be>
2024-03-07 11:20:09 +0100

    Split file

--- controle_transfermarkt.py
+++ controle_transfermarkt.py
@@ -38,25 +38,6 @@ team_name_mapping = {
     'Union SG': 'Union Saint-Gilloise'
 }
 
-for indx in goal_events_df.index:
-    goal_time = goal_events_df.at[indx, 'goal_time'][:-1]
-    if "+" in goal_time:
-        times = goal_time.split("+")
-        goal_events_df.at[indx, 'goal_time'] = str(sum(map(int, times)))
-    else:
-        goal_events_df.at[indx, 'goal_time'] = goal_time
-
-# Convert 'goal_time' column to numeric
-goal_events_df['goal_time'] = pd.to_numeric(goal_events_df['goal_time'])
-
-# Create TimedeltaIndex
-lijst_times = goal_events_df['time'] + pd.to_timedelta(goal_events_df['goal_time'], unit='m',)
-
-goal_events_df['real_time_goal'] = lijst_times.astype(str).map(lambda x: x[7:])
-
-goal_events_df.to_csv('csv/goal_events.csv', index=False)
-
-
 goal_events_df = goal_events_df[goal_events_df['valid_goal'] == 1]
 
 # Calculate goals for home and away teams
@@ -67,13 +48,12 @@ goals_away_team = goal_events_df[goal_events_df['goal_team'] == goal_events_df['
 goals_home_team_df = goals_home_team.reset_index(name='calculated_goals_home_team')
 goals_away_team_df = goals_away_team.reset_index(name='calculated_goals_away_team')
 
-# Merge the goal counts into the match results with outer join
+# Replace NaN values in goal columns with 0
 merged_df = pd.merge(match_results_df, goals_home_team_df, on='match_id', how='outer')
 merged_df = pd.merge(merged_df, goals_away_team_df, on='match_id', how='outer')
-
-# Replace NaN values in goal columns with 0
 merged_df['calculated_goals_home_team'] = merged_df['calculated_goals_home_team'].fillna(0)
 merged_df['calculated_goals_away_team'] = merged_df['calculated_goals_away_team'].fillna(0)
+
 
 # Calculate discrepancies
 discrepancies = merged_df[
